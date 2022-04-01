11a4eddf4eaea748b668f26417e4fef8552b10dc
Pooja Sree <you@example.com>
2022-04-01 18:45:26 +0530

    added: readme.MD file

---
+++ practiceproblem38.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 20;
+let c = a+b;
+console.log("Num Addition is : "+c);
+
+let s1 = "10";
+let s2 = "20";
+let s3 = "s1 + s2";
+console.log("str Addition is : "+s3);
+
+let d = new Date();   // creates a varibale of current date
+let today = "Today is: "+ (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
+let time  = "Time:" + d.getHours() + ":" + (d.getMinutes());
+console.log("Todays Date: " +today);
